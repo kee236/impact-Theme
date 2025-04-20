573af2d37abee870ac8d510e2fa678a60cc96e1e
kee236 <31869356+kee236@users.noreply.github.com>
2025-04-20 14:56:20 +0700

    Create PHP/scripts.php

---
+++ src/front/PHP/scripts.php
@@ -0,0 +1,13 @@
+<script src="<?php echo base_url('assets/vendor/jquery/dist/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/vendor/popper.js/dist/umd/popper.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/vendor/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/vendor/headroom.js/dist/headroom.min.js'); ?>"></script>
+
+<script src="<?php echo base_url('assets/vendor/onscreen/dist/on-screen.umd.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/vendor/waypoints/lib/jquery.waypoints.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/vendor/jarallax/dist/jarallax.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js'); ?>"></script>
+
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+
+<script src="<?php echo base_url('assets/js/front.js'); ?>"></script>

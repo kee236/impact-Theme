b049fbfdecaa7c2b0800d717cd7c5256b32272a8
kee236 <31869356+kee236@users.noreply.github.com>
2025-04-20 14:57:46 +0700

    Create preloader.php

---
+++ src/front/PHP/preloader.php
@@ -0,0 +1,6 @@
+<div class="preloader bg-soft flex-column justify-content-center align-items-center">
+    <div class="loader-element">
+        <span class="loader-animated-dot"></span>
+        <img src="<?php echo base_url('assets/img/brand/dark-loader.svg'); ?>" height="40" alt="<?php echo $this->config->item('product_name'); ?> logo">
+    </div>
+</div>
